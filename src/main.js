33fcb9c1f3ba4d047d3095584d4fa6034975fb6d
Hugo Benício <hbobenicio@gmail.com>
2018-01-06 09:56:45 -0300

    Adicionando as dependencias basicas (vuetify, vue-router, vuex e axios)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// Vuetify
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+
 import App from './App.vue'
+
+// Plugins
+Vue.use(VueRouter)
+Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
